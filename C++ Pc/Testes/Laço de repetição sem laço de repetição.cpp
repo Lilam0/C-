7cf9134efd5bf383596935d2a5312f7f2d1e8c9c
William Colasso <161973026+Lilam0@users.noreply.github.com>
2024-04-29 23:20:38 -0300

    Update Laço de repetição sem laço de repetição.cpp

--- C++ Pc/Testes/Laço de repetição sem laço de repetição.cpp
+++ C++ Pc/Testes/Laço de repetição sem laço de repetição.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-int num, num2, i = 0;
-
+int num, i = 0;
 int main() {
-	std::cout << "Digite uma amostra de numeros, em seguida digite um segundo numero. \nEntao descubra todos os numeros dentro desta amostra, que tem divisao com resto 0, com base no segundo número";
 	std::cin >> num;
-	std::cout << "Segundo numero: ";
-	std::cin >> num2;
 	div(0);
 }
 int div(int i) {
-	if (i % num == 0) {
+	if (i % 1000000 == 0) {
 		std::cout << "Numero: " << i << "\n";
 		i++;
 		div(i);
