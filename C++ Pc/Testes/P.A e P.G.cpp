c3c5c12c89503bace526f262d0b5dcdf13f986d8
William Colasso <161973026+Lilam0@users.noreply.github.com>
2024-05-01 16:43:29 -0300

    Update P.A e P.G.cpp

--- C++ Pc/Testes/P.A e P.G.cpp
+++ C++ Pc/Testes/P.A e P.G.cpp
@@ -5,7 +5,7 @@ using namespace std;
 using namespace std::this_thread;
 using namespace std::chrono;
 
-int chose, num1, num2, auxi, tempo = 5, auxi2 = 1, auxi3;
+int chose, num1, num2, auxi, tempo = 7, auxi2 = 1, auxi3;
 bool continuar = true, menu = true;
 
 void arithmeticProgression() {
@@ -27,6 +27,7 @@ void arithmeticProgression() {
         for (int i = tempo; i > 0; i--) {
             cout << i << "..";
             sleep_until(system_clock::now() + seconds(2));
+
         }
     }
     else {
@@ -49,14 +50,16 @@ void geometricProgression() {
         for (int i = 0; i < auxi3; i++) {
             auxi2 *= auxi;
         }
-        num1 = (num1*(auxi2-1))/(auxi-1)   ;
+        num1 = (num1 * (auxi2 - 1)) / (auxi - 1);
         cout << "The the sum of Geometric Progression is:\n";
         cout << num1 << "\n" << "Continuing in:";
 
         for (int i = tempo; i > 0; i--) {
             cout << i << "..";
             sleep_until(system_clock::now() + seconds(2));
+
         }
+        
     }
     else {
         menu = true;
@@ -64,15 +67,74 @@ void geometricProgression() {
 }
 
 void general() {
+    system("cls");
 
+    cout << "What's the first number of the sequence? (zero to exit)\n";
+    cin >> num1;
+    system("cls");
+
+    if (num1 != 0) {
+        cout << "What's number do you want to know?\n";
+        cin >> num2;
+        cout << "What's the ratio of the P.A?\n";
+        cin >> auxi;
+        num1 = num1+((num2-1)*auxi);
+        cout << "The the 'n' terme is:\n";
+        cout << num1 << "\n" << "Continuing in:";
+
+        for (int i = tempo; i > 0; i--) {
+            cout << i << "..";
+            sleep_until(system_clock::now() + seconds(2));
+
+        }
+        
+    }
+    else {
+        menu = true;
+    }
 }
 
 void razao() {
+    system("cls");
+    cout << "What's the first number of the sequence? (zero to exit)\n";
+    cin >> num1;
+    system("cls");
 
+    if (num1 != 0) {
+        cout << "What's second number of the sequence?\n";
+        cin >> num2;
+        num1 = num2/num1;
+        cout << "The the ratio is:\n";
+        cout << num1 << "\n" << "Continuing in:";
+
+        for (int i = tempo; i > 0; i--) {
+            cout << i << "..";
+            sleep_until(system_clock::now() + seconds(2));
+
+        }
+        
+    }
+    else {
+        menu = true;
+    }
 }
 
 void settings() {
+    system("cls");
+    cout << "Enter your choice: \n";
+    std::cout << "1-=======Waiting time===========\n";
+    cin >> chose;
+    system("cls");
+    if (chose == 1) {
+        cout << "Type a new waiting time";
+        cin >> tempo;
+        cout << "The new time is:" << tempo<<"\n";
+        for (int i = tempo; i > 0; i--) {
+            cout << i << "..";
+            sleep_until(system_clock::now() + seconds(2));
 
+        }
+    }
 }
 
 int main() {
@@ -84,43 +146,19 @@ int main() {
             std::cout << "2-=======Sum of Geometric Progression=====\n";
             std::cout << "3-=======Formula.A========================\n";
             std::cout << "4-=======Geometric Ratio==================\n";
-            std::cout << "5-=======Settings=========================\n";
-            std::cout << "6-=======Exit=============================\n";
+            std::cout << "5-=======Exit=============================\n";
             cin >> chose;
             system("cls");
         }
         menu = false;
-        switch (chose)
-        {
-        case 1:
-            arithmeticProgression();
-            
 
-        case 2:
-            geometricProgression();
-            
-
-        case 3:
-            general();
-            
-
-        case 4:
-            razao();
-            
-
-        case 5:
-            settings();
-            
-
-        case 6:
-            continuar = false;
-            menu = false;
-           
-
-        default:
-            cout << "Invalid Number!";
+        if (chose == 1) arithmeticProgression();
+        else if (chose == 2) geometricProgression();
+        else if (chose == 3) general();
+        else if (chose == 4) razao();
+        else if (chose == 5) break;
+        else {
             menu = true;
-            
         }
     }
 }
